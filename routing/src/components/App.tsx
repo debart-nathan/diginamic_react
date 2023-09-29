e9b1c220e499a40a72bd8a5fc1bdf32706e594a0
debart-nathan <denathan@laposte.net>
2023-09-29 11:36:49 +0200

    init react routing app

---
+++ routing/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <h1>Header commun</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
