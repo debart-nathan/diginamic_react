7b96f40c8d2f2a5655acbbda234f07f4b3b8b788
debart-nathan <denathan@laposte.net>
2023-09-29 12:08:43 +0200

    add nav using Link

--- routing/src/components/App.tsx
+++ routing/src/components/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import {Outlet} from "react-router-dom"
+import {Outlet,Link} from "react-router-dom"
 
 function App() {
     return (
         <div className="App">
             <header>
                 <h1>Header commun</h1>
+                <nav>
+                    <ul  className=" d-flex list-unstyled gap-2">
+                        <li>
+                            <Link to="/">Accueil</Link>
+                        </li>
+                        <li>
+                            <Link to="/articles">Article</Link>
+                        </li>
+                    </ul>
+                </nav>
             </header>
             <main className="container">
                 <Outlet/>
