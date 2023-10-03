8323afeaa662d404e53e5a630aed01a5ae02da4f
debart-nathan <denathan@laposte.net>
2023-10-03 13:33:02 +0200

    memopus : header styling

--- memopus/src/components/Header.tsx
+++ memopus/src/components/Header.tsx
@@ -7,9 +7,11 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ onLogout, isConnected }) => {
     return (
-        <header className='bg-dark'>
-            <h1>memopus</h1>
-            {isConnected && <button onClick={onLogout}>Log out</button>}
+        <header className='navbar navbar-dark bg-dark justify-content-center'>
+            <div className="container-fluid d-flex justify-content-center">
+                <h1 className="navbar-brand mb-0 h1 fw-bold">memopus</h1>
+                {isConnected && <button className="btn btn-outline-light position-absolute end-0">Log out</button>}
+            </div>
         </header>
     );
 }
